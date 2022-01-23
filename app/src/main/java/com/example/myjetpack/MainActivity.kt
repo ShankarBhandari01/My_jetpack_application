c699f2405123b3e3ae86082911dc629020bebcce
Shankar bhandari <shankarbhandari01>
2022-01-23 20:51:22 +0545

    for loop on row and column  elements

--- app/src/main/java/com/example/myjetpack/MainActivity.kt
+++ app/src/main/java/com/example/myjetpack/MainActivity.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myjetpack.ui.theme.Purple200
 
@@ -26,7 +29,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Display() {
-    Column {
+    Column(
+        modifier = Modifier
+            .background(color = Color.Yellow)
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.Center,
+        // horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        for(i in 0..50 )
         Text(
             text = "Hello This is JetPack",
             fontSize = 30.sp,
@@ -36,19 +48,21 @@ fun Display() {
                 .fillMaxWidth(),
             textAlign = TextAlign.Center
         )
-        SelectionContainer() {
-            Text(
-                text = "Hello This is JetPack ".repeat(10),
-                fontSize = 30.sp,
-                color = Purple200,
-                modifier = Modifier
-                    .background(color = Color.Yellow)
-                    .fillMaxWidth(),
-                textAlign = TextAlign.Center,
-                maxLines = 2
-            )
-        }
 
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .horizontalScroll(rememberScrollState()),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(text = "This is element 1")
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(text = "This is element 2")
+            Text(text = "This is element 1")
+            Spacer(modifier = Modifier.width(10.dp))
+            Text(text = "This is element 2")
+        }
 
     }
 
